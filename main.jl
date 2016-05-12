a1ee1a36f015ebf3d361d7b8fd3b94a3bd8bb2ff
Bas Beelen <bjgbeelen@gmail.com>
2016-05-12 08:01:29 +0200

    Initial example

---
+++ main.jl
@@ -0,0 +1,19 @@
+using JuMP
+using Cbc
+
+println("Hello Julia")
+
+m = Model(solver = CbcSolver(seconds=0))
+@variable(m, 0 <= x <= 2 )
+@variable(m, 0 <= y <= 30 )
+
+@objective(m, Max, 5x + 3*y )
+@constraint(m, 1x + 5y <= 3.0 )
+
+print(m)
+
+status = solve(m)
+
+println("Objective value: ", getobjectivevalue(m))
+println("x = ", getvalue(x))
+println("y = ", getvalue(y))
